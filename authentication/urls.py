864ef5d3bfc671f001fd6110c8d3935cf2743f81
Jake <bajentingjackie@gmail.com>
2024-02-10 17:16:15 +0800

    bruh

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,11 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
     path('anhs',views.anhs, name="anhs"),
     path('sign_up', views.signup_view, name="sign_up"),
-    path("login", views.login_view, name="login"),
-    
-    
-    
+    path("login_view", views.login_view, name="login_view"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -17,7 +17,7 @@ def login_view(request):
         user = authenticate(request,username = username, password= password)
         if user is not None:
             login(request,user)
-            return HttpResponseRedirect(reverse('map'))
+            return HttpResponseRedirect(reverse('home'))
         else:
             return render(request, "registration/login.html",{
                 "message": "Invalid credentials"

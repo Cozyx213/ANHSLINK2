6599f8dc32bec97802f3f1d7f1a945d3c14036c4
Athena <bajentingjackie@gmail.com>
2024-02-23 12:50:23 +0800

    added pythonanywhere for static url

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/pixelonlyplz/jake/ANHSLINK/static'
+
 
 LOGIN_REDIRECT_URL= '/home'
 LOGOUT_REDIRECT_URL= '/anhs'

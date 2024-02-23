d530ae11bc6ad84f39c3fd253a82f6af9adc7d15
Athena <bajentingjackie@gmail.com>
2024-02-23 12:55:07 +0800

    changed anhslink to 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/pixelonlyplz/jake/ANHSLINK/static'
+STATIC_ROOT = '/home/pixelonlyplz/jake/ANHSLINK2/static'
 
 
 LOGIN_REDIRECT_URL= '/home'

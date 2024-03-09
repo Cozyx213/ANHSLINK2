30856ac7e2ec17c0818d5b4b72a4696bd0912827
Jake <bajentingjackie@gmail.com>
2024-03-09 22:00:55 +0800

    changed the home

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     
-    path("home", views.home, name="home"),
+    path("home", views.library, name="home"),
     path("map", views.map, name="map"),
     path('history', views.history, name="history"),
     path('create_post', views.create_post, name='create_post'),

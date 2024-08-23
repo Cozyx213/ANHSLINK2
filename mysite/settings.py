e19177415f775d643c901b56fb730a5590dc5468
Jake <bajentingjake@gmail.com>
2024-08-23 18:35:34 +0800

    fixed cors issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'mysite.urls'
 CORS_ALLOWED_ORIGINS = [
     "http://192.168.1.6:3000",
     "http://localhost:3000",
-    "https://anhslink.netlify.app/",
+    "https://anhslink.netlify.app",
 ]
 
 TEMPLATES = [
